4e7044cda8783b81e2ca1946919f91b6e88bb263
Anthony Clerc <anthony.v.clerc@gmail.com>
2019-03-24 23:45:51 +0100

    All parts together

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -9,48 +9,90 @@
 //****************************************************************
 
 #include <LEDArray.h>
-#define NB_LEDS 8
+#include "pitches.h"
+
+#define NB_LEDS 16
+#define SPEAKER_PIN 11
+#define CAMERA_FOCUS_PIN 12
+#define CAMERA_SHUTTER_PIN 13
+#define BUTTON_PIN A0
+
 LEDArray ledArray;
 
 void setup() {
   //set pins to output so you can control the shift register
   Serial.begin(9600);
-  pinMode(0, OUTPUT);
-  digitalWrite(0,LOW);
-  pinMode(5,INPUT);
-
+  pinMode(CAMERA_FOCUS_PIN, OUTPUT);
+  pinMode(CAMERA_SHUTTER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); 
+  
   ledArray.setNbLEDs(NB_LEDS);
+  ledArray.setLedsState(0);
 
 }
 void loop() {
 
-if(digitalRead(5)){
 
-byte ledState = 0x00;
-for(size_t i = 0; i <NB_LEDS; i++)
-{
-  ledState = (ledState << 1) + 0b01;
-  ledArray.setLedsState(ledState);
+ if (fallingEdge(BUTTON_PIN) || true){
+
+    uint16_t ledState = 0x00;
+    for(size_t i = 0; i <NB_LEDS; i++)
+    {
+      ledState = (ledState << 1) + 0b01;
+      ledArray.setLedsState(ledState);
+      if ((i+1)%16 == 0){
+        tone(SPEAKER_PIN, NOTE_A4);
+        photo();
+      } else if ((i+1)%4==0) {
+        tone(SPEAKER_PIN,NOTE_A3);
+      }      
+      delay(300);
+      noTone(SPEAKER_PIN);
+    }
+    ledArray.setLedsState(0);
+  }
+
+}
+
+
+bool fallingEdge(uint8_t pin){
+  if (digitalRead(pin) == LOW) {
+    delay(10);
+    return !digitalRead(pin);
+  }
+  return false;
+}
+
+// photo trigger a photo shoot 
+// Both the focus and the shutter need to be set high to take a photo
+// It works in bulb mode which means that the first tigger start the shoot 
+// the second trigger stop the shoot.
+void photo(){
+  digitalWrite(CAMERA_FOCUS_PIN, HIGH);
+  digitalWrite(CAMERA_SHUTTER_PIN, HIGH);
+  delay(100);
+  digitalWrite(CAMERA_FOCUS_PIN, LOW);
+  digitalWrite(CAMERA_SHUTTER_PIN, LOW);
+
   delay(300);
-}
-digitalWrite(0,HIGH);
-delay(100);
-digitalWrite(0,LOW);
-delay(100);
-digitalWrite(0,HIGH);
-delay(100);
-digitalWrite(0,LOW);
-}
-
+  
+  digitalWrite(CAMERA_FOCUS_PIN, HIGH);
+  digitalWrite(CAMERA_SHUTTER_PIN, HIGH);
+  delay(100);
+  digitalWrite(CAMERA_FOCUS_PIN, LOW);
+  digitalWrite(CAMERA_SHUTTER_PIN, LOW);
+  return;
+  
 }
 
 
-
+volatile int I = 0;
 SIGNAL(TIMER0_COMPA_vect) 
 {
-  ledArray.ledCycle();
- 
-
-
+  if(I == 1){
+   ledArray.ledCycle();
+    I = 0;
+  }
+  I++;
 } 
 
